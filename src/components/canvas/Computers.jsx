db4318e88cd1ee0e5e1ce9f6731051336f4f2d01
Adam Firdaus <adam.firshah@gmail.com>
2023-11-20 19:42:42 +0800

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,15 @@
+// import { Suspense, useEffect, useState } from "react";
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, Prelaod, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from '../Loader';
+
+const Computers = () => {
+  // const computer = useGLTF('./dekstop_pc/scene.glft')
+
+  return (
+    <div>Computers</div>
+  )
+}
+
+export default Computers
